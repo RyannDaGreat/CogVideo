d847c55516607f529117339f32f06cb67566c19e
Ryan <ryancentralorg@gmail.com>
2024-10-16 10:07:19 +0000

    I don't know why this line appeared but its a big bug prevented it from using GPU silently

--- finetune/finetune_single_rank.sh
+++ finetune/finetune_single_rank.sh
@@ -10,7 +10,6 @@ export CACHE_PATH="~/.cache"
 # export DATASET_PATH="/root/CleanCode/Github/CogVideo/finetune/datasets/Disney-VideoGeneration-Dataset"
 export DATASET_PATH="/root/CleanCode/Github/CogVideo/finetune/datasets/Single-Sample-Disney-VideoGeneration-Dataset"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 
 # if you are not using wth 8 gus, change `accelerate_config_machine_single.yaml` num_processes as your gpu number
 accelerate launch --config_file accelerate_config_machine_single.yaml --multi_gpu \
